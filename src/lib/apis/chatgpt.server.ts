0f7f92961b0e27c7bc8f38ad1bd7b0c5b868dac7
Curtis Ruck <curtis@ruck.io>
2023-06-03 11:49:57 -0400

    improve UI

--- src/lib/apis/chatgpt.server.ts
+++ src/lib/apis/chatgpt.server.ts
@@ -9,7 +9,7 @@ export async function createCritique(text: string): Promise<string | undefined> 
     return openai.createChatCompletion({
         model: 'gpt-3.5-turbo',
         messages: [
-            {role: 'system', content: 'You are a copy editor, your task is to review text and provide praise and critique in order to assist the user improve the writing.'},
+            {role: 'system', content: 'You are a copy editor, your task is to review text and provide constructive criticism in order to assist the user improve the writing.'},
             {role: 'user', content: text},
         ],
         // prompt: 'The following is a text that I want you to analyze and provide feedback on.  Please provide praise and critiques of the following text: ' + text + '\n\nFeedback: ',
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,57 +6,74 @@
 </script>
 
 <div class="container">
-  <div class="writing">
-    <h2>Text</h2>
-    <form method="POST" action="/">
-      <textarea name="text" rows="20">{form?.prompt || ""}</textarea>
-      <button class="feedback" type="submit">Get Feedback</button>
-    </form>
-  </div>
-  <div class="critique">
-    <h2>Feedback</h2>
-    {#if form}
-    <SvelteMarkdown source={form?.response}/>
-    {/if}
+  <div class="panes">
+    <div class="pane writing">
+      <h2>Input Text</h2>
+      <form method="POST" action="/">
+        <textarea name="text" rows="20">{form?.prompt || ""}</textarea>
+        <button class="feedback" type="submit">Get Feedback</button>
+      </form>
+    </div>
+    <div class="pane critique">
+      <h2>Feedback</h2>
+      {#if form}
+      <SvelteMarkdown source={form?.response}/>
+      {/if}
+    </div>
   </div>
 </div>
 
 <style>
-    .container {
-        padding: 0rem;
+    .panes {
         display: flex;
-        flex-direction: row;
-        height: calc(100vh - 1rem);
-    }
-
-    .container div {
-        padding: 1rem;
-        display: inline-block;
-        width: 50%;
-        overflow: auto;
-    }
-
-    .writing textarea {
-        width: 100%;
         height: 100%;
+        padding: 20px;
         box-sizing: border-box;
-        margin: 0;
+    }
+
+    .pane {
+        flex: 1;
+        margin-right: 10px;
+        padding: 20px;
+        box-sizing: border-box;
+        background: #f9f9f9;
+        border-radius: 5px;
+        box-shadow: 0 0 10px rgba(0,0,0,0.1);
+    }
+
+    .pane:last-child {
+        margin-right: 0;
+    }
+
+    textarea, .critique {
+        width: 100%;
+        height: calc(100% - 70px);
+        margin-bottom: 10px;
+        padding: 10px;
+        box-sizing: border-box;
+        border-radius: 5px;
+        border: 1px solid #ddd;
+        font-size: 16px;
+        line-height: 1.5;
         resize: none;
     }
 
-    .critique {
-        padding: 4px;
-    }
-
-    .critique h2 {
-        text-align: right;
-    }
-
-    .feedback {
-        display: block;
-        margin: 0 auto;
+    button {
         width: 100%;
-        padding: 0.25em;
-        font-size: 1rem;
+        padding: 10px;
+        border: none;
+        border-radius: 5px;
+        background: #007BFF;
+        color: white;
+        font-size: 16px;
+        cursor: pointer;
+    }
+
+    button:hover {
+        background: #0056b3;
+    }
+
+    h2 {
+        margin-bottom: 10px;
     }
 </style>
